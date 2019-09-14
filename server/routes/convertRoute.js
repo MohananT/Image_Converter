d054e154f8a3dac35ff5cbcb0a4ed9a043798177
MohananT <mohan.stanky@gmail.com>
2019-09-14 12:12:20 +0530

    MohananT

---
+++ server/routes/convertRoute.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var tesseract = require('tesseract.js');
+
+// router handler
+var router = express.Router();
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.send('Success');
+// });
+
+router.get('/', function(req, res, next) {
+  // const filename = req.body.filename;
+  console.log('FileName from req', req.body.filename);
+  var filename = './routes/pic.PNG'
+  tesseract.recognize(filename)
+  .progress(function  (p) { console.log('progress', p)  })
+  .catch(err => {
+    console.error(err);
+    res.send('Error');
+  })
+  .then(function (result) {
+    console.log(result.text);
+    res.send('Success' + result.text);
+  })
+});
+
+module.exports = router;
